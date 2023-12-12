29eada6c12acdae346b8d8007b19e017d82581b1
André Luiz <dantasluizandre@gmail.com>
2023-12-12 12:46:50 -0300

    Rotação de imagens

--- js/comprar.js
+++ js/comprar.js
@@ -2,30 +2,36 @@
 const roupa = document.querySelector("#roupa")
 const valor = document.querySelector("#valor")
 const imagens = document.getElementsByClassName('img')   
+const imgGrande = document.getElementById('img4')
 
 window.addEventListener('load', e => {
     roupa.innerHTML = window.localStorage.getItem('nome')
     const valorComCifrao = `$${window.localStorage.getItem('valor')}`;
     valor.innerHTML = valorComCifrao;
-    window.localStorage.getItem('img')
-    window.localStorage.getItem('id')
-    console.log(imagens)
+  
     Array.from(imagens).forEach((element , i ) => {
      let imag = document.getElementById('img' + (i + 1))
      let url =   window.localStorage.getItem('img' + (i + 1))
         imag.src = url
-        console.log(url)
+       
     });                       
   }
 )
 
-function TrocarPosicao() {
-  var imagem = document.getElementById('img1');
-imagem.addEventListener('click' , e => {
+let grandona =  window.localStorage.getItem('img4')
+for (let i = 0; i < 3; i++) {
   
-  var destino = document.getElementById('img4');         
-  imagem.parentNode.removeChild(imagem);
-  destino.appendChild(imagemClonada);
-})}
-TrocarPosicao()
-// var imagemClonada = imagem.cloneNode(true);
+  let imagem = document.getElementById('img' + (i + 1))
+
+  imagem.addEventListener('click' , e => {
+    imgGrande.src = imagem.src
+    imagem.src = grandona
+    grandona = imgGrande.src
+
+
+  })}
+  
+
+
+
+
